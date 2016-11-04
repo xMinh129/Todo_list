50809a51233397c3b0b3606e4dd4d90c6197fda0
Minh Vu <xuanminh12995@gmail.com>
2016-11-04 23:54:04 +0800

    Change  todoitem

--- db/migrate/20161103173039_create_todo_items.rb
+++ db/migrate/20161103173039_create_todo_items.rb
@@ -2,6 +2,7 @@ class CreateTodoItems < ActiveRecord::Migration[5.0]
   def change
     create_table :todo_items do |t|
       t.string :content
+      t.date :date
       t.references :todo_list, foreign_key: true
 
       t.timestamps
